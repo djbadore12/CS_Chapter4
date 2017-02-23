4b461d6c791cd6b15dbef8130d39ba67062cbe76
djbadore12 <djbadore12@yahoo.com>
2017-02-23 14:15:10 -0500

    sdf

--- CS_Chapter4/TubSales/Program.cs
+++ CS_Chapter4/TubSales/Program.cs
@@ -10,6 +10,23 @@ namespace TubSales
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Which sales person did you have?");
+
+            String inputString = Console.ReadLine();
+
+            while (inputString != "A")
+            {
+                int a = 0;
+                Console.WriteLine("How much was the sales worth?");
+                int cost = Console.Read();
+                Console.WriteLine(cost);
+
+            }
+
+
+            
+           
+
         }
     }
 }
